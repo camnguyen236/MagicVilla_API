a83db08b6cf21a8f30ae3e781d1d3ea217872298
camnguyen2306 <cam2362002@gmail.com>
2023-04-08 17:34:11 +0700

    add CRUD villa 🥰

--- MagicVilla/Program.cs
+++ MagicVilla/Program.cs
@@ -1,8 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
+
+//builder.Services.AddControllers().AddJsonOptions(options =>
+//{
+//    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+//    options.JsonSerializerOptions.DictionaryKeyPolicy = null;
+//});
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
